17be9b85efe2318eee9c532c2d7b9d1f895d22b7
quartz55 <john.razor97@gmail.com>
2021-02-05 15:45:25 +0000

    Updated deps

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        # some symlink/relative path shenanigans happening 🤷
+        esy = pkgs.writeShellScriptBin "esy" "${pkgs.nodePackages.esy}/lib/node_modules/.bin/esy $@";
+      in
+      rec {
+        packages ={
+          relog = pkgs.callPackage ./. { inherit pkgs; };
+        };
+        defaultPackage = packages.relog;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            libsodium
+            esy
+            ocamlformat
+            ocamlPackages.ocaml-lsp
+            ocamlPackages.reason
+            ocamlPackages.dune_2
+          ];
+        };
+      }
+    );
+}
